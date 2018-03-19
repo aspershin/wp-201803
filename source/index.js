c253a214e4c405cc6b6a281f4f5dfca006bfeab7
Andrey Pershin <aspershi@gmail.com>
2018-03-19 20:35:14 +0300

    20180319-2035

---
+++ source/index.js
@@ -0,0 +1,3 @@
+import createMenu from './menu';
+let menu = createMenu(['Главная','Обо мне','Блог'], 'menu');
+document.body.appendChild(menu);
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const PATHS = {
+    source: path.join(__dirname, 'source'),
+    build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+    entry: PATHS.source + '/index.js',
+    output: {
+        path: PATHS.build,
+        filename: '[name].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({title: 'webpack app'}),
+        new CleanWebpackPlugin('build')
+    ]
+};
